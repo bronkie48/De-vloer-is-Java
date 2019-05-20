8c077df2e90e8e203ff9aa3f5ad26e340c11bba6
Robinvsa <robinvsa@hotmail.nl>
2019-05-20 13:45:33 +0200

    Robin: H11Q4, H11Q8, H12Q1

---
+++ src/H11_Quiz4/SuperB.java
@@ -0,0 +1,13 @@
+package H11_Quiz4;
+
+public class SuperB{
+    protected int x = 3;
+
+    public SuperB( ){
+        x += 2;
+        System.out.print(" x" + x);
+    }
+}
+
+
+
---
+++ src/H11_Quiz8/Superklasse.java
@@ -0,0 +1,8 @@
+package H11_Quiz8;
+
+public class Superklasse{
+    public String st1 = "1";
+    protected String str2 = "2";
+    String str3 = "3";
+    private String str4 = "4";
+}
