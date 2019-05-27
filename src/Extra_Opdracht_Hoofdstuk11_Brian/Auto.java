2c78186054cec1af888604e1b41fa5053be34a20
Brian Bronkhorst <brianbronkhorst@upcmail.nl>
2019-05-27 14:06:20 +0200

    Brian: Eigen opdracht H11

---
+++ src/Extra_Opdracht_Hoofdstuk11_Brian/Auto.java
@@ -0,0 +1,10 @@
+package Extra_Opdracht_Hoofdstuk11_Brian;
+
+public class Auto extends Voertuigen{
+	
+	public void infoMethode() {
+		System.out.println("--Auto--");
+		super.infoMethode();
+		System.out.println("--------");
+	}
+}
