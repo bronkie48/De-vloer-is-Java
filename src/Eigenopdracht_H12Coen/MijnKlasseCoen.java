676f346737224308734aeeadcdb0a8b60df59e38
Coen Okken <coenokken@hotmail.com>
2019-06-03 12:52:57 +0200

    Coen Okken

---
+++ src/Eigenopdracht_H12Coen/MijnKlasseCoen.java
@@ -0,0 +1,13 @@
+package Eigenopdracht_H12Coen;
+
+public class MijnKlasseCoen extends SuperKlasseCoen {
+	int z = 5;
+	void mijnMethode(int x)
+	{
+		System.out.print("x");
+	}
+	public static void main(String[] args)
+	{
+		MijnKlasseCoen mk = new MijnKlasseCoen();
+	}
+}
