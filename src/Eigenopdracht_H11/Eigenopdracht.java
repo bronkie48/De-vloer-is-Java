28b9cd5d443c1b0e2090c6041f5092c23322b2f1
Thomas <buitenhuis87@gmail.com>
2019-06-03 13:30:59 +0200

    3-6-2019

--- src/Eigenopdracht_H11/Eigenopdracht.java
+++ src/Eigenopdracht_H11/Eigenopdracht.java
@@ -5,12 +5,16 @@ public class Eigenopdracht extends EigenklasseApp{
 	public static void main (String [] args) {
 		Eigenopdracht eo = new Eigenopdracht();
 		eo.mijnMethodeBrian(1, 1);
+		Eigenopdracht thomas = new Eigenopdracht();
+		thomas.thomasMethode();
 	}
 
 	int mijnMethodeBrian(int y, int x) {
 		return brianMethode(y) + x + y;
 	}
-
-
-}
+	
+	void thomasMethode(char c) {
+		return;
+		} 
+	}
 
